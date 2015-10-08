2e983e0e52e3284c91da02f319e8ea022c37764b
MichaelWeedmark <michael.weedmak@ocsbstudent.ca>
2015-10-08 14:47:34 -0400

    Walking Animation

---
+++ WalkingAnimation/WalkingAnimation.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WalkingAnimation
+{
+    partial class frmAnimation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picWalkingMan = new System.Windows.Forms.PictureBox();
+            this.btnRunAnimation = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWalkingMan
+            // 
+            this.picWalkingMan.Image = global::WalkingAnimation.Properties.Resources.walk1;
+            this.picWalkingMan.Location = new System.Drawing.Point(151, 52);
+            this.picWalkingMan.Name = "picWalkingMan";
+            this.picWalkingMan.Size = new System.Drawing.Size(180, 180);
+            this.picWalkingMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picWalkingMan.TabIndex = 0;
+            this.picWalkingMan.TabStop = false;
+            // 
+            // btnRunAnimation
+            // 
+            this.btnRunAnimation.Location = new System.Drawing.Point(204, 257);
+            this.btnRunAnimation.Name = "btnRunAnimation";
+            this.btnRunAnimation.Size = new System.Drawing.Size(75, 23);
+            this.btnRunAnimation.TabIndex = 1;
+            this.btnRunAnimation.Text = "button1";
+            this.btnRunAnimation.UseVisualStyleBackColor = true;
+            this.btnRunAnimation.Click += new System.EventHandler(this.btnRunAnimation_Click);
+            // 
+            // frmAnimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 306);
+            this.Controls.Add(this.btnRunAnimation);
+            this.Controls.Add(this.picWalkingMan);
+            this.Name = "frmAnimation";
+            this.Text = "Animation";
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWalkingMan;
+        private System.Windows.Forms.Button btnRunAnimation;
+    }
+}
+
---
+++ WalkingAnimation/WalkingAnimation.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WalkingAnimation
+{
+    public partial class frmAnimation : Form
+    {
+        public frmAnimation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRunAnimation_Click(object sender, EventArgs e)
+        {
+            int loopcounter = 0;
+            do
+            {
+                if (loopcounter == 0)
+                {
+                    this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk2;
+                }
+                else if (loopcounter == 1)
+
+                    
+                    {
+                        this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk3;
+                    }
+                    else if (loopcounter == 2)
+
+                       
+                        {
+                            this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk4;
+                        }
+                        else if (loopcounter == 3)
+
+                           
+                            {
+                                this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk5;
+                            }
+                            else if (loopcounter == 4)
+
+                               
+                                {
+                                    this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk6;
+                                }
+                                else if (loopcounter == 5)
+
+                                    
+                                    {
+                                        this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk7;
+                                    }
+                                    else if (loopcounter == 6)
+
+                                        
+                                        {
+                                            this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk8;
+                                        }
+                                        else if (loopcounter == 7)
+
+                                           
+                                            {
+                                                this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk9;
+                                            }
+                                            else if (loopcounter == 8)
+
+                                                
+                                                {
+                                                    this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk10;
+                                                }
+                                                else if (loopcounter == 9)
+
+                                                   
+                                                    {
+                                                        this.picWalkingMan.Image = WalkingAnimation.Properties.Resources.walk1;
+                                                    }
+
+                 loopcounter = loopcounter + 1;
+
+                    // pauses the animation
+                    this.Refresh();
+                    Thread.Sleep(100);
+
+                } while (loopcounter < 10);
+            }
+    }
+}
